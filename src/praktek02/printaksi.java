cfdd13bba140ebaf98759210941aed0032b7b3a5
Anggawiranata <32449107+Anggawiranata@users.noreply.github.com>
2018-03-31 15:01:06 +0800

    mengisi nilai atribut

--- src/praktek02/printaksi.java
+++ src/praktek02/printaksi.java
@@ -5,6 +5,9 @@ package praktek02;
 public class printaksi {
     public static void main(String[] args) {
         print a1 = new print();
+        a1.merk = "canon";
+        a1.harga = 650;
+        a1.jumlah = 2;
         
     }
 }
